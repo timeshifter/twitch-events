1140eb314fbddeff6e6fb324c963851dd95a6671
timeshifter <chrono34@hotmail.com>
2018-05-01 14:43:24 -0500

    added readme, a few events formalized

---
+++ README.md
@@ -0,0 +1,73 @@
+#Twitch Events
+
+###written by timeshifter
+
+This is a JavaScript library that intends to consolidate and simplify all events that are relevant to a Twitch chat, i.e. followers, subscribes, etc.
+
+It exposes every event available, but JOIN and PART events appear to be sent out in recurring batches, and I've seen delays up to several minutes before a JOIN event is sent. This library will not attempt to short-circuit those events; they are exposed as they are dispatched.
+
+=====
+
+##Current implementation notes:
+
+Chat rooms are currently only supported by full room ID, i.e. `chatrooms:61927669:a0c1e70d-7d52-491a-bcd5-6db6a5492d64`. This will be streamlined in a future version.
+
+=====
+
+####Initialization
+
+```javascript
+		var client = new TwitchClient({
+			ClientID: 'abcde12345',
+			Nick: 'botshifter08',
+			Pass: 'oauth:54321edcba',
+			Debug: true
+		});
+
+		client.JoinChannels('timeshifter08');
+```
+
+#####Parameters
+
+| Parameter | Notes |
+| --- | --- |
+|  ClientID | Your app's Client ID as provided by Twitch |
+| Nick/Pass | The nickname/password you want the library to connect as. Required for joining rooms. |
+| Debug | Enables a complete WebSocket log to the console. Defaults to `false`. |
+
+#####Methods
+
+| Method | Notes |
+| --- | --- |
+| `Connect()` | Initializes the WebSocket connection with the credentials specified on instantiation. Should never need to be called manually; the library will automatically attempt to reconnect if any disruptions occur. |
+| `JoinChannels(obj)` | Join the specified channel(s). Accepts a `String` or `Array` of channel names/room ID's. |
+| `LeaveChannels(obj)` | Leave the specified channel(s). Accepts a `String` or `Array` of channel names/room ID's. |
+| `SendMessage(channel, message)` | Sends a message to the specified channel/room ID. Cannot send to channels you haven't joined. |
+
+####Default event signatures
+
+```javascript
+		client.onMessage = function (message) {
+			console.log('onMessage', { 'Message': message, 'Timestamp': new Date() });
+		};
+
+		client.onPrivmsg = function (user, channel, message) {
+			console.log('onPrivmsg', { 'Username': user, 'Channel': channel, 'Message': message, 'Timestamp': new Date() });
+		};
+
+		client.onJoin = function (user, channel) {
+			console.log('onJoin', { 'Username': user, 'Channel': channel, 'Timestamp': new Date() });
+		};
+
+		client.onPart = function (user, channel) {
+			console.log('onPart', { 'Username': user, 'Channel': channel, 'Timestamp': new Date() });
+		};
+
+		client.onRoomstate = function (channel, settings) {
+			console.log('onRoomstate', { 'Channel': channel, 'Settings': settings, 'Timestamp': new Date() });
+		};
+
+		client.onUsernotice = function (message) {
+			console.log(message);
+		};
+		```
--- twitch-events.js
+++ twitch-events.js
@@ -16,13 +16,16 @@ function TwitchClient(opts) {
         _clientId = opts.ClientID,
         _nick,
         client = this,
-        _defaultEventsHandled = {};
+        _defaultEventsHandled = {},
+        _channelIds = {},
+        _channelNames = {},
+        _channelRooms = {};
 
 
     //publics
     this.Debug = opts.Debug || false;
     this.Channels = [];
-
+    this.PendingChannels = [];
 
     if (_clientId == undefined && this.Debug) {
         console.log('ClientID not provided; follower data will not be available.');
@@ -37,7 +40,20 @@ function TwitchClient(opts) {
             this.send(`NICK ${opts.Nick}`);
         }
         else {
-            this.send(`NICK justinfan ${Math.floor(Math.random() * 999999)}`);
+            this.send(`NICK justinfan${Math.floor(Math.random() * 999999)}`);
+        }
+
+        var idx=0;
+        while (idx < client.PendingChannels.length) {
+            if (client.PendingChannels[idx].indexOf(':') >= 0) {
+                var c = client.PendingChannels[idx];
+                this.send(`JOIN ${c}`);
+                client.Channels[c] = {};
+                client.PendingChannels.shift();
+            }
+            else {
+                idx++
+            }
         }
     }
 
@@ -54,7 +70,8 @@ function TwitchClient(opts) {
                 console.log('>' + line);
             }
 
-            client.onMessage(line);
+            if (client.onMessage)
+                client.onMessage(line);
 
             var parts = line.split(' ');
 
@@ -62,29 +79,103 @@ function TwitchClient(opts) {
                 _ws.send(`PONG ${parts[1].substring(1)}`);
             }
             else if (parts[1] == 'JOIN') {
+                if (client.onJoin)
+                    client.onJoin(parts[0].split(':')[1].split('!')[0], parts[2]);
                 //fireEvent('onJoin', line);
             }
             else if (parts[1] == 'PART') {
+                if (client.onPart)
+                    client.onPart(parts[0].split(':')[1].split('!')[0], parts[2]);
                 //fireEvent('onPart', line);
             }
             else if (parts[1] == 'MODE') {
                 //fireEvent('onMode', line);
             }
             else if (parts[2] == 'PRIVMSG') {
-                var colonSplit = line.split(':');
-                var user = colonSplit[1].split('!')[0],
-                    channel = colonSplit[1].split('#')[1].trim();
 
-                client.onPrivmsg(user, channel, colonSplit.shift().shift().join(':'));
+/*
 
-                //fireEvent('onPrivmsg', line);
+@badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=d653a0d0-1cd0-492e-ad6e-2de1940a8d16;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1524771643982;turbo=0;user-id=61927669;user-type=
+ :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG #timeshifter08 :asdf
+
+@badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=847906fc-5db5-4ce5-9258-f206b94d9a31;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1524771400844;turbo=0;user-id=61927669;user-type=
+ :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG #chatrooms:61927669:a0c1e70d-7d52-491a-bcd5-6db6a5492d64 :asdf
+
+*/
+
+
+                var spaceSplit = line.split(' ');
+
+                var user = spaceSplit[1].split(':')[1].split('!')[0],
+                    channel = spaceSplit[3];
+                
+                var idx = line.indexOf(':', line.indexOf(channel));
+                var msg = line.substring(idx + 1);
+                
+
+                if (client.onPrivmsg)
+                    client.onPrivmsg(user, channel.substring(1), msg);
+
             }
             else if (parts[2] == 'ROOMSTATE') {
-                //this.InitChannel(line);
-                //fireEvent('onRoomstate', line);
+
+                /*
+
+@broadcaster-lang=;emote-only=0;followers-only=-1;r9k=0;rituals=1;room-id=61927669;slow=0;subs-only=0 :tmi.twitch.tv ROOMSTATE #timeshifter08
+
+
+@badges=broadcaster/1;color=#1E90FF;display-name=timeshifter08;emotes=;id=377f9462-5f5e-4e5a-a3c5-13911dc7a623;mod=0;room-id=61927669;subscriber=0;tmi-sent-ts=1525115130064;turbo=0;user-id=61927669;user-type= :timeshifter08!timeshifter08@timeshifter08.tmi.twitch.tv PRIVMSG
+ #chatrooms:61927669:a0c1e70d-7d52-491a-bcd5-6db6a5492d64 :alooooha
+
+
+                */
+
+                var parts = line.split(' ');
+                var userdata = parts[0].split(';');
+
+                var channel = line.split(' ')[3].substring(1).trim().toLowerCase();
+
+                //channel record has already been created, bail
+                if (_channelIds[channel] != undefined) {
+                    return;
+                }
+
+                for (var i = 0; i < userdata.length; i++) {
+                    if (userdata[i].indexOf('room-id') == 0) {
+                        _channelIds[channel] = userdata[i].split('=')[1];
+                        _channelNames[userdata[i].split('=')[1]] = channel;
+                        _channelRooms[userdata[i].split('=')[1]] = {};
+                        break;
+                    }
+                }
+
+
             }
             else if (parts[2] == 'USERNOTICE') {
-                //fireEvent('onUsernotice', line);
+
+                client.onUsernotice(line);
+
+                var msgParts = parts[0].split(';');
+
+                for (var i = 0; i < msgParts.length; i++) {
+
+                    if (msgParts[i] == 'msg-id=sub') {
+                        isSub = 0;
+                        break;
+                    }
+                    else if (msgParts[i] == 'msg-id=resub') {
+                        isSub = 1;
+                        break;
+                    }
+                    else if (msgParts[i] == 'msg-id=subgift') {
+                        isSub = 2;
+                        break;
+                    }
+
+                }
+
+
+
             }
             else {
                 //console.log('>' + line);
@@ -97,20 +188,31 @@ function TwitchClient(opts) {
             console.log(`Websocket error: ${e}`);
         }
         setTimeout(function () {
-            InitWebsocket();
+            client.Connect();
         }, 1000);
     }
 
     function OnWebsocketClose(e) {
         setTimeout(function () {
-            InitWebsocket();
+            client.Connect();
         }, 1000);
     }
+
+
+
+
+
 
 
     //public functions
 
     this.Connect = function () {
+
+        for (c of client.Channels) {
+            client.PendingChannels.push(c);
+        }
+        client.Channels = [];
+
         _ws = new WebSocket('wss://irc-ws.chat.twitch.tv');
         _ws.onopen = OnWebsocketOpen;
         _ws.onmessage = OnWebsocketMessage;
@@ -138,33 +240,102 @@ function TwitchClient(opts) {
 
         for (c of arr) {
             c = c.trim().toLowerCase();
-            this.Channels.push(c);
+
+            //if (c.split(':').length == 3) {
+            //    throw 'Error: Chat rooms must be joined by <channel name>:<room name>.'
+            //    return;
+            //}
+            //else if (c.split(':').length == 2) {
+
+
+            //}
+
+
             if (c[0] != '#')
                 c = '#' + c;
-            _ws.send(`JOIN ${c}`);
+
+            if (_ws.readyState == 1) {
+                client.Channels.push(c);
+
+                _ws.send(`JOIN ${c}`);
+            }
+            else
+                client.PendingChannels.push(c);
+
+        }
+    }
+
+    ///Leave a single channel or an array of channel names
+    this.LeaveChannels = function (channels) {
+        var arr = [];
+        if (!Array.isArray(channels)) {
+            arr.push(channels);
+        }
+        else {
+            arr = channels;
+        }
+
+        for (c of arr) {
+            c = c.trim().toLowerCase();
+            if (c[0] != '#')
+                c = '#' + c;
+
+            if (_ws.readyState == 1) {
+
+                _ws.send(`PART ${c}`);
+                var i = client.Channels.indexOf(c);
+                client.Channels.splice(i, 1);
+            }
+            else {
+                var i = client.PendingChannels.indexOf(c);
+                client.PendingChannels.splice(i, 1);
+
+            }
         }
     }
 
 
     //events
 
-    this.onMessage = function (message) {
-        if (!_defaultEventsHandled.onMessage) {
-            console.log('onMessage event not handled!');
-            _defaultEventsHandled.onMessage = true;
+    function SetDefaultEventHandled(evt) {
+        if (!_defaultEventsHandled[evt]) {
+            console.log(`${evt} event not handled!`);
+            _defaultEventsHandled[evt] = true;
         }
+    }
+
+    this.onMessage = function (message) {
+        SetDefaultEventHandled('onMessage');
     }
 
     this.onPrivmsg = function (user, channel, message) {
-        if (!_defaultEventsHandled.onPrivmsg) {
-            console.log('onPrivmsg event not handled!');
-            _defaultEventsHandled.onPrivmsg = true;
-        }
+        SetDefaultEventHandled('onPrivmsg');
+    }
+
+    this.onJoin = function (user, channel) {
+        SetDefaultEventHandled('onJoin');
+    }
+
+    this.onPart = function (user, channel) {
+        SetDefaultEventHandled('onPart');
+    }
+
+    this.onRoomstate = function (channel, settings) {
+        SetDefaultEventHandled('onRoomstate');
+    }
+
+    this.onUsernotice = function (message) {
+        SetDefaultEventHandled('onUsernotice');
     }
 
 
 
+    ///gooooo!
     this.Connect();
 
-    
+
+    this.DebugFunction = function () {
+        console.log('channel id list:   ', _channelIds);
+        console.log('channel name list: ', _channelNames);
+    }
 }
